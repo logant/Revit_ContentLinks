2e6c39b535134f056fdb23cb82d59673684418ba
tlogan <tlogan@PWDA010>
2018-07-30 14:07:42 -0500

    Adding 'HKS, Inc.' to assembly's Company attribute and adding code signing to post build event.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LINE.UpdateableLinks")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Elk")]
 [assembly: AssemblyProduct("LINE.UpdateableLinks")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
